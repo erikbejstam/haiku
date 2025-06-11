6bd6ba5a24f48d04be90f4541b10b7fa31ba528b
erikbejstam <erikbejstam@hotmail.com>
2025-06-11 10:48:51 +0200

    Update SQL insert statements.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO HAIKU (author_name, text, created_at)
-VALUES ('Erik', 'Detta är en fin dikt', '2024-05-13 14:30:00');
-
 INSERT INTO HAIKUUSER (id, username, password, email)
 VALUES (1, 'u', 'p', 'u@u.com');
+
+INSERT INTO HAIKU (user_id, text, created_at)
+VALUES (1, 'Detta är en fin dikt', NOW());
