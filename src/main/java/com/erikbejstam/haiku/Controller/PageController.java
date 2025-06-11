8935e7247f51be5d96d239b6c84fe3a0d46ceafc
erikbejstam <erikbejstam@hotmail.com>
2025-06-11 09:37:03 +0200

    Update PageController to send in haikus to homepage.

--- src/main/java/com/erikbejstam/haiku/Controller/PageController.java
+++ src/main/java/com/erikbejstam/haiku/Controller/PageController.java
@@ -1,13 +1,27 @@
 package com.erikbejstam.haiku.Controller;
 
+import com.erikbejstam.haiku.Model.Haiku;
+import com.erikbejstam.haiku.Service.HaikuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PageController {
 
+    @Autowired
+    HaikuService haikuService;
+
     @GetMapping("/")
-    public String home() {
-       return "main";
+    public String home(Model model) {
+        List<Haiku> haikus = haikuService.findAll();
+
+        model.addAttribute("haikus", haikus);
+
+        return "main";
     }
 }
