beedcbc8aedcd867492adbded34a77daef00c55f
erikbejstam <erikbejstam@hotmail.com>
2025-06-11 13:25:03 +0200

    Update SQL data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO HAIKUUSER (id, username, password, email)
-VALUES (1, 'u', 'p', 'u@u.com');
+VALUES (1, 'u', '$2y$10$k9LVr7aIYvy0/JNL/9odAOs4AIiK/kjmLDNQSBpjBns9RIixXNfZ2', 'u@u.com');
 
 INSERT INTO HAIKU (user_id, text, created_at)
 VALUES (1, 'Detta är en fin dikt', NOW());
