c139809838dc9e44e53190311cb002dba8f79494
erikbejstam <erikbejstam@hotmail.com>
2025-06-29 11:39:59 +0200

    Add script for navigating between haikus on home page.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,8 +1,27 @@
-const button = document.getElementById('menu-button');
+const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 
-button.addEventListener('click', () => {
-    const isExpanded = button.getAttribute('aria-expanded') === 'true';
-    button.setAttribute('aria-expanded', String(!isExpanded));
+const upArrow = document.getElementById('up-arrow');
+const downArrow = document.getElementById('down-arrow');
+const haikus = document.querySelectorAll('.haiku');
+let currentIndex = 0;
+
+// Toggle menu items
+
+menuButton.addEventListener('click', () => {
+    const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+    menuButton.setAttribute('aria-expanded', String(!isExpanded));
     menu.classList.toggle('show');
 });
+
+// Navigate to next/previous haiku
+
+function showHaiku(index) {
+    haikus[currentIndex].classList.add('hidden');
+    currentIndex =  (index + haikus.length) % haikus.length;
+    haikus[currentIndex].classList.remove('hidden');
+}
+
+upArrow.addEventListener('click', () => showHaiku(currentIndex + 1));
+downArrow.addEventListener('click', () => showHaiku(currentIndex - 1));
+
